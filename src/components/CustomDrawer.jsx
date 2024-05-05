2d6f31a8a099b6a726042beaaed4abcbd679df86
razannael <nailrazan40@gmail.com>
2024-05-05 14:12:15 +0300

    Tell a friend btn

--- src/components/CustomDrawer.jsx
+++ src/components/CustomDrawer.jsx
@@ -4,6 +4,8 @@ import {
   Text,
   Image,
   TouchableOpacity,
+  Share,
+  Alert,
 } from 'react-native';
 import {
   DrawerContentScrollView,
@@ -13,6 +15,28 @@ import Colors from '../utils/Colors.js';
 import { AntDesign } from '@expo/vector-icons';
 
 const CustomDrawer = props => {
+
+
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message:
+          'I am using I Am Alive app. Download it from Play Store. https://play.google.com/store/apps/details?id=com.iamalive.iamalive',
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  };
+
 
   return (
     <View style={{flex: 1}}>
@@ -42,7 +66,7 @@ const CustomDrawer = props => {
         </View>
       </DrawerContentScrollView>
       <View style={{padding: 20, borderTopWidth: 1, borderTopColor: '#ccc',marginBottom:40}}>
-        <TouchableOpacity onPress={() => {}} style={{paddingVertical: 10}}>
+        <TouchableOpacity onPress={onShare} style={{paddingVertical: 10}}>
           <View style={{flexDirection: 'row', alignItems: 'center', gap:3}}>
           <AntDesign name="sharealt" size={18} color="gray" />
             <Text
